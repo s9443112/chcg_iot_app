3481afd3fe49131278c502c56793ee1845b0d5da
s9443112 <s9443112@gmail.com>
2025-08-05 01:30:23 +0800

    add google-service

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -22,10 +23,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.agritalk_iot_app"
+        applicationId = "com.agritalk.agritalk_iot_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,27 @@ import 'pages/login_page.dart';
 import 'pages/home_page.dart'; // <-- 要引入！
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('🔴 背景收到通知: ${message.messageId}');
+}
+
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await FirebaseMessaging.instance.requestPermission();
+  
+  String? token = await FirebaseMessaging.instance.getToken();
+  print('✅ FCM Token: $token');
+
   runApp(
     ResponsiveSizer(
       builder: (context, orientation, screenType) {
@@ -24,7 +42,7 @@ class MyApp extends StatelessWidget {
       title: 'AgriTalk IoT',
       theme: ThemeData(
         primaryColor: const Color(0xFF7B4DBB),
-colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF7B4DBB)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF7B4DBB)),
         fontFamily: 'SourceHanSansTW',
         useMaterial3: true, // Material 3！
       ),
