fc9fc3dc2316fbec547bc3f81f3f5dcbf9f56590
s9443112 <s9443112@gmail.com>
2025-10-09 10:32:07 +0800

    fix error

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -76,7 +76,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('assets/logo-01.png', width: 180),
+                Image.asset('assets/logo_icon.png', width: 180),
                 const SizedBox(height: 24),
                 TextField(
                   controller: usernameController,
